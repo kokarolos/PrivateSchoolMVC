3d010703612987498dd047964ba74d0d109bd3de
kokarolos <kokarolos@gmail.com>
2020-03-29 10:37:33 +0300

    Seeding Method Updated Views Tavbles fixed

--- PrivateSchool.Desktop/Program.cs
+++ PrivateSchool.Desktop/Program.cs
@@ -1,8 +1,5 @@
-﻿using PrivateSchool.Database;
-using PrivateSchool.Entities.Concrete;
-using PrivateSchool.Entities.Intermediaries;
+﻿using System;
 using PrivateSchool.Services;
-using System;
 
 namespace PrivateSchool.Desktop
 {
@@ -11,11 +8,30 @@ namespace PrivateSchool.Desktop
         static void Main(string[] args)
         {
             CourseRepository cs = new CourseRepository();
+            StudentRepository ss = new StudentRepository();
+            AssignmentRepository ass = new AssignmentRepository();
+            TrainerRepository ts = new TrainerRepository();
+
+
             var courses = cs.GetCourses();
+            var trainers = ts.GetTrainers();
+            var assignments = ass.GetAssignments();
+            var students = ss.GetStudents();
             foreach (var course in courses)
             {
-
                 Console.WriteLine(course.Stream);
+            }
+            foreach (var trainer in trainers)
+            {
+                Console.WriteLine(trainer.FirstName);
+            }
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.FirstName);
+            }
+            foreach (var assignment in assignments)
+            {
+                Console.WriteLine(assignment.Description);
             }
         }
     }
